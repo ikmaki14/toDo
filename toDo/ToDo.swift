85fefd0e13172e2fec56eed5e553c140759b0ff2
Isabella <ikmaki14@gmail.com>
2020-07-13 01:21:56 -0500

    Completed Iteration 1

---
+++ toDo/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDo
+//
+//  Created by Isabella Maki on 7/13/20.
+//  Copyright © 2020 Isabella Maki. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ toDo/ToDoTableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ToDoTableViewController.swift
+//  toDo
+//
+//  Created by Isabella Maki on 7/13/20.
+//  Copyright © 2020 Isabella Maki. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var toDos : [ToDo] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDos = createToDos()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+
+        return cell
+    }
+
+    /*
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func createToDos() -> [ToDo] {
+        
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        // important is set to false by default
+        
+        return [swift, dog]
+    }
+
+}
